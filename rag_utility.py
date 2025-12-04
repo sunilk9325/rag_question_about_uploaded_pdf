a9ed51bd5232b06b2222a4661500aaf3a0e5d7ab
sunilk9325 <42057293+sunilk9325@users.noreply.github.com>
2025-12-04 17:39:33 +0530

    Fix import path for RetrievalQA

--- rag_utility.py
+++ rag_utility.py
@@ -7,7 +7,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
-from langchain_community.chains import RetrievalQA
+from langchain.chains import RetrievalQA
 
 
 # Load environment variables from .env file
